5e60eacd0443fa23f347aea81334bc9f19113888
Esc9919 <Emersoncaldeira1999@hotmail.com>
2023-10-29 16:39:47 -0300

    tentativa falha de integrar o login com o html

--- BarsiSistemaRh/Barsi.Web/Program.cs
+++ BarsiSistemaRh/Barsi.Web/Program.cs
@@ -1,6 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/login", async context =>
+    {
+        context.Response.ContentType = "text/html";
+        await context.Response.SendFileAsync("C:\\Users\\Public\\Barsi-Sistema-RH\\BarsiSistemaRH\\BarsiSistemaRh\\Barsi.Web\\HTML\\sua-pagina-login.html");
+    });
+
+});
 
 app.Run();
---
+++ BarsiSistemaRh/BarsiSistemaRh/Controllers/WebLoginController.cs
@@ -0,0 +1,37 @@
+﻿using Barsi.Api.Models.Dtos.FuncionarioDtos;
+using Barsi.Api.Services.LoginService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Barsi.Web.Controllers;
+
+public class WebLoginController : Controller
+{
+    private readonly ILoginService _loginService;
+
+    public WebLoginController(ILoginService loginService)
+    {
+        _loginService = loginService;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Acessar(LoginDto loginDto)
+    {
+        var resultado = _loginService.Acessar(loginDto);
+
+        if (resultado is OkObjectResult)
+        {
+            return View("PáginaDeSucesso"); 
+        }
+        else
+        {
+            return View("PáginaDeErro");
+        }
+    }
+}
